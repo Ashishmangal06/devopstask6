fa660b517f2da41c0d8f02f22f6bfdfc7e73a279
Ashishmangal06 <59526244+Ashishmangal06@users.noreply.github.com>
2020-06-25 17:17:46 +0530

    groovy file

---
+++ jenkins.groovy
@@ -0,0 +1,69 @@
+job("job1") {
+
+
+  description("Creates the image")
+  
+  steps {
+  
+   scm {
+     git {
+       extensions {
+         wipeWorkspace()
+       }
+     }
+   }
+   scm {
+     github("Ashishmangal06/devopstask6", "master")
+   }
+   shell("sudo cp -rvf * /root/devopstask6;if ls | grep php;then sudo cp /root/devopstask6/image/php/Dockerfile /root/devopstask6/;else sudo cp /root/devopstask6/image/html/Dockerfile /root/devopstask6/;fi")
+   shell("sudo docker build -t ashishmangal/website:latest /root/devopstask6/;sudo docker push ashishmangal/website:latest")  
+  }
+}
+
+
+
+job("job2") {
+  description("Deploying job") 
+  triggers {
+    upstream {
+      upstreamProjects("job1")
+      threshold("SUCCESS")
+    }  
+  }
+  steps {
+      shell("if sudo kubectl get pods | grep mypod; then sudo kubectl delete pods --all; fi;if sudo kubectl get service | grep myservice ; then sudo kubectl delete service --all; fi;if sudo kubectl get pvc | grep mywebpvc; then sudo kubectl delete pvc --all; fi;sudo kubectl create -f /root/devopstask6/mypvc.yml;sudo kubectl create -f /root/devopstask6/deploy.yml;sudo kubectl create -f /root/devopstask6/service.yml;sudo kubectl get all;") 
+  }
+}
+
+
+
+job("job3") {
+  description("Monitoring Job")
+  triggers {
+     scm("* * * * *")
+   }
+  steps {
+    shell("sudo cd /root/devopstask6")
+    shell('export status=$(curl -siw "%{http_code}" -o /dev/null 192.168.99.100:30000); if [ $status -eq 200 ]; then exit 0; else python3 sendmail.py; exit 1; fi')
+  }
+}
+
+
+job("job4") {
+  description("Redeploying Job")
+  triggers {
+    upstream {
+      upstreamProjects("job3")
+      threshold("FAILURE")
+    }
+  }
+  publishers {
+    postBuildScripts {
+      steps {
+        downstreamParameterized {
+  	  	  trigger("job1")
+        }
+      }
+    }
+  }
+}
